c674b6fd3bff6223a0e161369ddc41a7de300c79
RustamGimadiev <gimadiev.kzn@yandex.ru>
2022-01-28 15:55:57 +0300

    add module

---
+++ modules/database/main.tf
@@ -0,0 +1,15 @@
+resource "aws_db_instance" "db" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = var.db_name
+  username             = "admin"
+  password             = random_password.db.result
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+
+resource "random_password" "db" {
+  length = 16
+}
