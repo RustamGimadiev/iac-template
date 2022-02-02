0b8a74268602edd0ff7518ee73588d12fe3aac7b
RustamGimadiev <gimadiev.kzn@yandex.ru>
2022-02-02 12:24:25 +0300

    Add file generation

--- modules/do-nothing/main.tf
+++ modules/do-nothing/main.tf
@@ -3,3 +3,8 @@ resource "null_resource" "this" {
     command = "echo 'Hello Terraform!'"
   }
 }
+
+resource "local_file" "this" {
+  filename = "${path.root}/empty.json"
+  content  = jsonencode({})
+}
